fa7aa77380be78e4b83a11042c4617dda800dd12
student <123@>
2023-08-04 12:38:34 +0530

    commit

--- app/src/main/java/com/example/mad3_63_b6/MainActivity2.kt
+++ app/src/main/java/com/example/mad3_63_b6/MainActivity2.kt
@@ -1,11 +1,16 @@
 package com.example.mad3_63_b6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+    fun launchsecondactivity(view: View){
+        intent= Intent(this,ManiActivity2)
     }
 }
