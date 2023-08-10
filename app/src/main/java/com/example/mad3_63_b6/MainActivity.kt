3aebdf5cbce81980b3ca551a033da6fb9e6edc99
student <student@gnu.ac.in>
2023-08-10 12:20:59 +0530

    commit

--- app/src/main/java/com/example/mad3_63_b6/MainActivity.kt
+++ app/src/main/java/com/example/mad3_63_b6/MainActivity.kt
@@ -1,18 +1,33 @@
 package com.example.mad3_63_b6
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     val tag="MainActivity"
+    lateinit var mmessage_edit_text:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val EXTRAMESSAGE="com.example.mad3_63_b6.message"
+        //same as int a;
+        mmessage_edit_text=findViewById(R.id.edit_text) //for finding edit_text in xml file
     }
     // calll alter + enter
-    fun SecondActivitycall(view: View) {
-        Log.i(tag," is called")
+
+
+    fun SecondActivity(view: View) {
+        //Log.i(tag,"called")
+
+        val intent = Intent(this, MainActivity2::class.java)
+        intent.putExtra("MainActivity","EXTRAMESSAGE")
+        var message=mmessage_edit_text.text.toString()
+        startActivity(intent)
     }
+//    val EXTRAMESSAGE="com.example.mad3_63_b6.message"
+//    lateinit var messagedittext:EditText
 }
--- app/src/main/java/com/example/mad3_63_b6/MainActivity2.kt
+++ app/src/main/java/com/example/mad3_63_b6/MainActivity2.kt
@@ -4,13 +4,17 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class MainActivity2 : AppCompatActivity() {
+
+    lateinit var text_header:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        val intent=intent //here we get intent from main activity
+        val bundle:String?=intent.getStringExtra("EXTRAMESSAGE")
+        findViewById(R.id.import_text)
     }
-    fun launchsecondactivity(view: View){
-        intent= Intent(this,ManiActivity2)
-    }
+
 }
