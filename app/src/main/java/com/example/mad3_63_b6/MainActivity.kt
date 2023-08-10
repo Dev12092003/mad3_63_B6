1d1a80a0d78c5b7fe7c195b892254664e940ebd8
student <student@gnu.ac.in>
2023-08-10 12:36:31 +0530

    commit

--- app/src/main/java/com/example/mad3_63_b6/MainActivity.kt
+++ app/src/main/java/com/example/mad3_63_b6/MainActivity.kt
@@ -10,10 +10,11 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     val tag="MainActivity"
     lateinit var mmessage_edit_text:EditText
+    val EXTRAMESSAGE="com.example.mad3_63_b6.message"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val EXTRAMESSAGE="com.example.mad3_63_b6.message"
+
         //same as int a;
         mmessage_edit_text=findViewById(R.id.edit_text) //for finding edit_text in xml file
     }
@@ -24,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         //Log.i(tag,"called")
 
         val intent = Intent(this, MainActivity2::class.java)
-        intent.putExtra("MainActivity","EXTRAMESSAGE")
+        intent.putExtra("MainActivity",EXTRAMESSAGE)
         var message=mmessage_edit_text.text.toString()
         startActivity(intent)
     }
--- app/src/main/java/com/example/mad3_63_b6/MainActivity2.kt
+++ app/src/main/java/com/example/mad3_63_b6/MainActivity2.kt
@@ -5,17 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
 
-    lateinit var text_header:EditText
+    lateinit var text_header:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         val intent=intent //here we get intent from main activity
-        val bundle:String?=intent.getStringExtra("EXTRAMESSAGE")
+        val bundle:String?=intent.getStringExtra("MainActivity")
         text_header=findViewById(R.id.import_text)
-        text_header.text=
+        text_header.text=bundle
     }
 
 }
